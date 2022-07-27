b2c16e2bb01c239693535d5cf785322931b1e06f
NightSea <sweetmilkshake0324@gmail.com>
2022-07-27 14:24:43 +0800

    feat: create search

---
+++ src/app/hero-search/hero-search.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+// RxJS
+import { Observable, Subject } from 'rxjs';
+import { debounceTime, distinctUntilChanged, switchMap } from 'rxjs/operators';
+
+// Interface
+import { Hero } from '../hero.interface';
+
+// Srv
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-hero-search',
+  templateUrl: './hero-search.component.html',
+  styleUrls: ['./hero-search.component.css'],
+})
+export class HeroSearchComponent implements OnInit {
+  heroes$!: Observable<Hero[]>;
+  private searchTerms = new Subject<string>();
+
+  constructor(private heroSrv: HeroService) {}
+
+  ngOnInit(): void {
+    this.heroes$ = this.searchTerms.pipe(
+      debounceTime(0),
+      distinctUntilChanged(),
+      switchMap((term: string) => this.heroSrv.searchHeroes(term))
+    );
+  }
+
+  /**
+   * search -
+   */
+  search(term: string): void {
+    this.searchTerms.next(term);
+  }
+}
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -84,4 +84,21 @@ export class HeroService {
       catchError(this.handleError<Hero>('deleteHero'))
     );
   }
+
+  /**
+   * searchHeros - 搜尋英雄
+   */
+  searchHeroes(term: string): Observable<Hero[]> {
+    if (!term.trim()) {
+      return of([]);
+    }
+    return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+      tap((x) =>
+        x.length
+          ? this.log(`found heroes matching "${term}"`)
+          : this.log(`no heroes matching "${term}"`)
+      ),
+      catchError(this.handleError<Hero[]>('searchHeroes', []))
+    );
+  }
 }
